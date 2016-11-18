9aae6313fe81c8ebf10022a0c62404f541643516
PrajktaP <prajkta.p@gmail.com>
2016-11-18 19:05:30 +0530

    removed unnecessary code

--- equilibrium_indices_in_array.rb
+++ equilibrium_indices_in_array.rb
@@ -1,43 +1,3 @@
-puts "hello prajkta"
-
-def show_equilibrium_indexes_for(arr, n)
-arr = arr
-n = n
-lsum = 0
-rsum = 0
-puts "For #{arr}"
-
-arr.each_with_index do |a, i|
-    lsum = arr[0..i-1].inject(0, :+)
-    rsum = arr[i+1..n].inject(0, :+)
-    puts lsum==rsum ? "#{i} is an equilibrium index" : "#{i} is not an equilibrium index"
-end  
-end
-
-arr = [-1, 3, -4, 5, 1, -6, 2, 1]
-show_equilibrium_indexes_for(arr, arr.size)
-
-
-def solution(a)
-  arr = a
-  n = arr.size
-  lsum = 0
-  rsum = 0
-  ei = []
-  
-  arr.each_with_index do |a, i|
-    lsum = arr[0..i-1].inject(0, :+)
-    rsum = arr[i+1..n].inject(0, :+)
-    ei << i if lsum==rsum
-  end
-  
-  puts "equilibrium indices : #{ei.empty? ? -1 : ei[0]}"
-  return ei.empty? ? -1 : ei[0]
-end
-solution([-1, 3, -4, 5, 1, -6, 2, 1])
-
-solution([-1, -3, -4, -5, -1, -6, -2, -1])
-
 def solution(a)
   arr = a
   n = arr.size
@@ -56,6 +16,5 @@ def solution(a)
     ei << i if lsum==rsum || add == true
   end
   
-  puts "equilibrium indices : #{ei.empty? ? -1 : ei[0]}"
   return ei.empty? ? -1 : ei[0]
 end
